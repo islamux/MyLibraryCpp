6906fb7355064de57da64b3fc596c3867269b182
islamux <fathi733@gmail.com>
2022-11-26 07:41:21 +0300

    add ready functions

---
+++ read_struct_number.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+  stNumber ReadStNumber(){
+
+    stNumber num;
+    cout << "Enter a number: " ;
+    cin >> num.num1;
+
+    cout << "Enter a number: " ;
+    cin >> num.num2;
+
+    return num;
+}
+
+
