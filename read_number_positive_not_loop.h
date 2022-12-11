bc72243fca1c8b5aad95b432c6efc997a6ec0fd7
islamux <fathi733@gmail.com>
2022-12-11 07:36:18 +0300

    add func

---
+++ read_number_positive_not_loop.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumberPositiveNotLoop(string msgToUsr){
+
+  int number = 0 ;
+
+  cout << msgToUsr << "\n";
+  cin >> number;
+  
+
+  return number;
+}
