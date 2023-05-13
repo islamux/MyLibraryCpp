a81427515514ca269d3b15ff5c574b85e3964bb7
islamux <fathi733@gmail.com>
2023-05-13 07:38:58 +0300

    copy arr

---
+++ copy_arr.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void CopyArray(int sourcArr[100],int lengthOfArr,int destArr[100], int& lengthArr2){
+
+    for(int i = 0; i < lengthOfArr; i++){
+      
+         
+          destArr[i] = sourcArr[i];
+         
+
+    }
+        
+    
+}
